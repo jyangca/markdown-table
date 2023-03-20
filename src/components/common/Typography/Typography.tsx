e95ba40fc66b87234eb5c70193b51e03214d6a7e
Jiho YANG <yjsm111@nuua.ai>
2023-03-20 22:02:34 +0900

    FEAT: Typography

---
+++ src/components/common/Typography/Typography.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import fontStyle, {
+  defaultTextStyle,
+  FontStyleKeyType,
+} from '@/theme/fontStyle';
+import { TypoElement, TypographyProps } from './Typography.types';
+
+const TextElement = styled.div<
+  TypographyProps & Required<Pick<TypographyProps, 'fontType'>>
+>`
+  text-align: ${(props) => props.textAlign};
+  ${(props) => fontStyle[props.fontType]}
+  ${defaultTextStyle};
+
+  ${({ noWrap }) =>
+    noWrap &&
+    css`
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    `}
+
+  ${({ decoration }) =>
+    decoration &&
+    css`
+      text-decoration: ${decoration};
+    `}
+
+  ${({ wordBreak }) =>
+    wordBreak &&
+    css`
+      word-break: ${wordBreak};
+    `}
+`;
+
+const defaultElement: { [key in FontStyleKeyType]: string } = {
+  h1R: 'h1',
+  h1B: 'h1',
+  h2R: 'h2',
+  h2B: 'h2',
+  h3R: 'h3',
+  h3B: 'h3',
+  h4R: 'h4',
+  h4B: 'h4',
+  h5R: 'h5',
+  h5B: 'h5',
+  pR12: 'div',
+  pB12: 'div',
+  pR20: 'div',
+  pB20: 'div',
+  spanR12: 'div',
+  spanB12: 'div',
+  spanR20: 'div',
+  spanB20: 'div',
+  captionR12: 'div',
+  captionB12: 'div',
+  captionR20: 'div',
+  captionB20: 'div',
+  miniR12: 'div',
+  miniB12: 'div',
+  miniR20: 'div',
+  miniB20: 'div',
+};
+
+const Typography = ({
+  fontType = 'spanR12',
+  tag = defaultElement[fontType] as TypoElement,
+  children,
+  style,
+  color = 'system3',
+  decoration,
+  ...props
+}: TypographyProps) => {
+  return (
+    <TextElement
+      as={tag}
+      fontType={fontType}
+      style={style}
+      color={color}
+      decoration={decoration}
+      {...props}
+    >
+      {children}
+    </TextElement>
+  );
+};
+
+export default Typography;
---
+++ src/components/common/Typography/Typography.types.ts
@@ -0,0 +1,43 @@
+import React, { HTMLAttributes } from 'react';
+import { FontStyleKeyType, TextStyleProperties } from '@/theme/fontStyle';
+
+type WordBreakType = 'normal' | 'break-all' | 'keep-all';
+
+export type TypoElement =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'p'
+  | 'div'
+  | 'span';
+
+export type TypographyProps = {
+  /** 폰트 타입을 지정합니다. */
+  fontType?: FontStyleKeyType;
+
+  /** HTML 태그를 지정할 수 있습니다. */
+  tag?: TypoElement;
+
+  /** 문자의 정렬을 지정합니다. */
+  textAlign?: string;
+
+  /** 컴포넌트의 className */
+  className?: string;
+
+  /** Typography 안의 내용 */
+  children?: React.ReactNode | React.ReactNode[];
+
+  /** ``true``로 설정 시 줄바꿈하지 않고 ...처리를 합니다. */
+  noWrap?: boolean;
+
+  style?: React.CSSProperties;
+
+  /** text-decoration */
+  decoration?: string;
+
+  wordBreak?: WordBreakType;
+} & TextStyleProperties &
+  HTMLAttributes<HTMLElement>;
