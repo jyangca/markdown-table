8462686783c0ea8587baeeb69bc90b36464c13e2
jyangca <jyangca@connect.ust.hk>
2023-03-23 23:38:49 +0900

    UPDATE: reorder TableForm css

--- src/components/TableForm/TableForm.style.ts
+++ src/components/TableForm/TableForm.style.ts
@@ -2,27 +2,18 @@ import styled from 'styled-components';
 
 export const TableAreaContainer = styled.div`
   width: 100%;
+  min-width: 500px;
+
   border-radius: 8px;
   border: none;
-  min-width: 500px;
+
   background-color: ${({ theme }) => theme.color.systemWhite};
 `;
 
 export const Table = styled.table`
   width: 100%;
-  border-collapse: collapse;
-`;
 
-type StyledCellProps = {
-  dragOver: boolean;
-};
-export const Cell = styled.td<StyledCellProps>`
-  text-transform: capitalize;
-  vertical-align: center;
-  padding: 1rem;
-  border-bottom: 1px solid ${({ theme }) => theme.color.systemE};
-  border-left: ${({ dragOver, theme }) =>
-    dragOver && `1px dashed ${theme.color.systemA}`};
+  border-collapse: collapse;
 `;
 
 type StyledThProps = {
@@ -30,17 +21,20 @@ type StyledThProps = {
 };
 export const StyledTh = styled.th<StyledThProps>`
   white-space: nowrap;
-  color: ${({ theme }) => theme.color.system9}
+  color: ${({ theme }) => theme.color.system9};
   letter-spacing: 1.5px;
   font-weight: 600;
   font-size: 16px;
   text-transform: capitalize;
+  text-transform: uppercase;
+
   vertical-align: middle;
+
   padding: 1rem 0;
   margin: 1rem;
-  border-bottom: 2px solid ${({ theme }) => theme.color.systemE};
-  text-transform: uppercase;
+
   border-radius: 8px;
+  border-bottom: 2px solid ${({ theme }) => theme.color.systemE};
   border-left: ${({ dragOver, theme }) =>
     dragOver && `1px dashed ${theme.color.systemA}`};
 
