d7e8888ba2d978a4c1d35d4b637386160fbc06ad
Jiho YANG <yjsm111@nuua.ai>
2023-03-31 18:15:29 +0900

    FEAT:  Add PasteForm component and its styles

---
+++ src/components/PasteForm/PasteForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PasteFormBox, PasteFormContainer } from './PasteForm.style';
+
+const PasteForm = () => {
+  return (
+    <PasteFormContainer>
+      <PasteFormBox></PasteFormBox>
+    </PasteFormContainer>
+  );
+};
+
+export default PasteForm;
