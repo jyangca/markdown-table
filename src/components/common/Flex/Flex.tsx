84dba59fefff2b0af045ca3df2eab7146257c0f1
Jiho YANG <yjsm111@nuua.ai>
2023-03-31 21:57:47 +0900

    FEAT: Create Flex component with dynamic props for flexible layout design

---
+++ src/components/common/Flex/Flex.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { FlexAlign, flexAlignMap, FlexDirection, flexDirectionMap, FlexGap, FlexJustify, flexJustifyMap, FlexProps } from './Flex.types';
+
+type FlexContainerProps = {
+  width?: string | number;
+  align: FlexAlign;
+  justify: FlexJustify;
+  direction: FlexDirection;
+  gap?: FlexGap;
+  boxWrap?: boolean;
+  boxFill?: boolean;
+};
+const FlexContainer = styled.div<FlexContainerProps>`
+  ${({ width }) =>
+    width &&
+    css`
+      width: ${width};
+    `}
+  ${({ boxFill }) =>
+    boxFill &&
+    css`
+      width: 100%;
+    `}
+
+  display: flex;
+  ${({ align }) => flexAlignMap[align]};
+  ${({ justify }) => flexJustifyMap[justify]};
+  ${({ direction }) => flexDirectionMap[direction]};
+
+  ${({ gap }) =>
+    gap?.column &&
+    css`
+      column-gap: ${gap.column}px;
+    `}
+  ${({ gap }) =>
+    gap?.row &&
+    css`
+      row-gap: ${gap.row}px;
+    `}
+
+  flex-wrap: ${({ boxWrap }) => (boxWrap ? 'wrap' : 'nowrap')};
+`;
+
+const Flex = ({
+  children,
+  align = 'CENTER',
+  justify = 'START',
+  direction = 'ROW',
+  style,
+  className,
+  id,
+  tag = 'div',
+  wrap,
+  ...argsProps
+}: FlexProps) => {
+  return (
+    <FlexContainer
+      as={tag}
+      align={align}
+      justify={justify}
+      direction={direction}
+      style={style}
+      className={className}
+      id={id}
+      boxWrap={wrap}
+      {...argsProps}
+    >
+      {children}
+    </FlexContainer>
+  );
+};
+
+export default Flex;
