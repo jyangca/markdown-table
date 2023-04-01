a26fc724c2c24fcc0b07cf101a72c9cc101eb584
Jiho YANG <yjsm111@nuua.ai>
2023-04-02 01:18:33 +0900

    UPDATE: Rearrange export order and add Flex component to components index file.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 export { default as TableForm } from './TableForm/TableForm';
-export { default as Typography } from './common/Typography/Typography';
-export { default as Button } from './common/Button/Button';
 export { default as Cell } from './Cell/Cell';
 export { default as Input } from './Input/Input';
 export { default as MarkdownContainer } from './MarkdownContainer/MarkdownContainer';
 export { default as HeaderCell } from './HeaderCell/HeaderCell';
 export { default as PasteForm } from './PasteForm/PasteForm';
+
+export { default as Button } from './common/Button/Button';
+export { default as Typography } from './common/Typography/Typography';
+export { default as Flex } from './common/Flex/Flex';
