404f6ec32cd0d2ca3f1cdd027ba951e234fbb848
Jiho YANG <yjsm111@nuua.ai>
2023-04-02 01:18:11 +0900

    UPDATE: Add copy button styling and functionality to MarkdownContainer in a concise and elegant way

--- src/components/MarkdownContainer/MarkdownContainer.style.ts
+++ src/components/MarkdownContainer/MarkdownContainer.style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Flex from '../common/Flex/Flex';
+import Button from '../common/Button/Button';
 
 export const MarkdownWrapper = styled(Flex)`
   padding: 1rem;
@@ -13,4 +14,13 @@ export const MarkdownStyle = styled(Flex)`
   padding: 0.5rem;
   border-radius: 4px;
   border: 1.5px solid ${({ theme }) => theme.color.system5};
+`;
+
+type CopyButtonProps = {
+  copied: boolean;
+};
+export const CopyButton = styled(Button)<CopyButtonProps>`
+  transition: all 0.25s ease-in-out;
+
+  background-color: ${({ theme, copied }) => (copied ? theme.color.systemA : theme.color.system3)};
 `;
