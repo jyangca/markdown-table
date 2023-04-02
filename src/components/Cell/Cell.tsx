c0323abda43d918e3de287c89d18fbbfe8536a3c
Jiho YANG <yjsm111@nuua.ai>
2023-04-02 23:33:48 +0900

    UPDATE:   Add click event to cell for input focus in edit mode

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -17,11 +17,15 @@ const Cell = ({ isEdit, children, updateMarkdown }: CellProps) => {
     updateMarkdown();
   };
 
+  const handleCellClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (isEdit) (event.target as HTMLDivElement).querySelector('input')?.focus();
+  };
+
   return (
-    <StyledTd className={toClassName(['cell', isEdit ? 'cell-mode-edit' : 'cell-mode-read'])}>
+    <StyledTd onClick={handleCellClick} className={toClassName(['cell', isEdit ? 'cell-mode-edit' : 'cell-mode-read'])}>
       {isEdit ? <Input onChange={handleChangeInput}>{value}</Input> : value}
     </StyledTd>
   );
 };
 
-export default memo(Cell);
+export default Cell;
