f576a503d67f6625b311e83a73ffb2e81d2406ba
Jiho YANG <yjsm111@nuua.ai>
2023-04-05 09:48:37 +0900

    UPDATE: Add new styles for PasteForm including a PasteFormInput component and a PreviewBox component with custom styling

--- src/components/PasteForm/PasteForm.style.ts
+++ src/components/PasteForm/PasteForm.style.ts
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
+import Flex from '../common/Flex/Flex';
+import Input from '../Input/Input';
 
-export const PasteFormContainer = styled.div`
+export const PasteFormContainer = styled(Flex)`
   padding: 2rem;
-  width: 100%;
   min-height: 300px;
 `;
 
 export const PasteFormBox = styled.textarea`
   width: 100%;
   height: 100%;
+
+  border-radius: 4px;
+  border: 1.5px solid ${({ theme }) => theme.color.system5};
+`;
+
+export const PreviewBox = styled(Flex)`
+  padding: 1rem;
+  height: 100%;
+  border-radius: 4px;
+  border: 2px solid ${({ theme }) => theme.color.system5};
+
+  user-select: none;
+`;
+
+export const PasteFormInput = styled(Input)`
+  width: 100px;
+  height: 32px;
 `;
