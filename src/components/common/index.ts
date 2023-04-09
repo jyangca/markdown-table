bd37213d76cb3580cb5c2823e82d5bebc3086483
Jiho YANG <yjsm111@nuua.ai>
2023-04-10 01:44:18 +0900

    UPDATE:  Refactor Popover component and add it to the common exports

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as Typography } from './Typography/Typography';
 export { default as Flex } from './Flex/Flex';
 export { default as Portal } from './Portal/Portal';
 export { default as Input } from './Input/Input';
+export { default as Popover } from './Popover/Popover';
