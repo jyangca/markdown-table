463f9a3541e0dc0c2e2e9de52a7703ff7bf2990c
Jiho YANG <yjsm111@nuua.ai>
2023-04-10 02:28:32 +0900

    FEAT: Add table snapshot state to capture current state of table when editing mode is toggled in TableForm and TableButtonList components

--- src/components/TableButtonList/TableButtonList.tsx
+++ src/components/TableButtonList/TableButtonList.tsx
@@ -13,23 +13,28 @@ const TableButtonList = ({ editMode, pasteMode, tableApi }: TableButtonListProps
   return (
     <Flex justify="SPACE_BETWEEN" boxFill>
       <Flex gap={{ column: 8 }}>
+        <Popover content={<NewPopover />}>
+          <Button disabled={pasteMode || editMode}>New</Button>
+        </Popover>
+        <Popover content={<NewPopover />}>
+          <Button disabled={pasteMode || editMode}>History</Button>
+        </Popover>
+
         <Button disabled={!editMode} onClick={tableApi?.handleAddColumn}>
           Add Column
         </Button>
         <Button disabled={!editMode} onClick={tableApi?.handleAddRow}>
           Add Row
         </Button>
-        <Button disabled={editMode || pasteMode} onClick={tableApi?.handleExportCsv}>
-          Export CSV
-        </Button>
+
         <Button disabled={pasteMode} onClick={tableApi?.handleChangeEditMode}>
           {editMode ? 'View' : 'Edit'}
         </Button>
-        <Popover content={<NewPopover />}>
-          <Button>New</Button>
-        </Popover>
       </Flex>
       <Flex direction="ROW" gap={{ column: 8 }}>
+        <Button disabled={editMode || pasteMode} onClick={tableApi?.handleExportCsv}>
+          Export CSV
+        </Button>
         <Button disabled={editMode} onClick={() => tableApi?.handleChangePasteMode({ isCancel: false })}>
           {pasteMode ? 'Done' : 'Paste'}
         </Button>
--- src/components/TableForm/TableForm.tsx
+++ src/components/TableForm/TableForm.tsx
@@ -36,6 +36,7 @@ const TableForm = ({ updateMarkdown }: TableFormProps) => {
   const [editMode, setEditMode] = useState<boolean>(false);
   const [pasteMode, setPasteMode] = useState<boolean>(false);
   const [tableApi, setTableApi] = useState<TableApiType>();
+  const [tableSnapshot, setTableSnapshot] = useState<{ cols: ColsType; rows: RowsType }>({ cols, rows });
 
   const updateRows = (newRows: RowsType) => {
     setRows(newRows);
@@ -72,6 +73,7 @@ const TableForm = ({ updateMarkdown }: TableFormProps) => {
     const { cols: newCols, rows: newRows } = removeEmptyRowAndCol({ rows: getCurrentRows(), cols: getCurrentCols() });
     updateRows(newRows);
     updateCols(newCols);
+    setTableSnapshot({ cols: newCols, rows: newRows });
     setEditMode((prev) => !prev);
     updateMarkdown();
   };
