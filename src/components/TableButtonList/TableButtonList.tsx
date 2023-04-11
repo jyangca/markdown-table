7f779e7c9505fca4ddfca653d9dd9621f1779748
Jiho YANG <yjsm111@nuua.ai>
2023-04-11 17:55:42 +0900

    UPDATE: Add Modal open logic on TableButtonList

--- src/components/TableButtonList/TableButtonList.tsx
+++ src/components/TableButtonList/TableButtonList.tsx
@@ -1,7 +1,9 @@
-import React, { memo } from 'react';
-import { Flex, Button, Popover } from '@/components/common';
+import React, { memo, useEffect, useState } from 'react';
+import { Flex, Button, Popover, Modal } from '@/components/common';
 import { HistoryPopover, NewPopover } from '@/components';
 import { TableApiType, TableHistoryType } from '@/types/common';
+import { setCookie } from '@/utils/common';
+import Cookies from 'js-cookie';
 
 type TableButtonListProps = {
   editMode: boolean;
@@ -11,36 +13,56 @@ type TableButtonListProps = {
 };
 
 const TableButtonList = ({ editMode, pasteMode, tableApi, tableHistory }: TableButtonListProps) => {
-  return (
-    <Flex justify="SPACE_BETWEEN" boxFill>
-      <Flex gap={{ column: 8 }}>
-        <Popover content={<NewPopover tableApi={tableApi} />}>
-          <Button disabled={pasteMode || editMode}>New</Button>
-        </Popover>
-        <Popover content={<HistoryPopover tableApi={tableApi} tableHistory={tableHistory} />} closeOption={{ keyDown: true, contentClick: true }}>
-          <Button disabled={pasteMode || editMode}>History</Button>
-        </Popover>
-        <Button disabled={!editMode} onClick={() => tableApi?.handleAddColumn()}>
-          Add Column
-        </Button>
-        <Button disabled={!editMode} onClick={() => tableApi?.handleAddRow()}>
-          Add Row
-        </Button>
+  const [modalOpen, setModalOpen] = useState(false);
 
-        <Button disabled={pasteMode} onClick={tableApi?.handleChangeEditMode}>
-          {editMode ? 'View' : 'Edit'}
-        </Button>
+  useEffect(() => {
+    const visited = Cookies.get('visited');
+    if (!visited) {
+      setCookie('visited', 'true', 30);
+      setModalOpen(true);
+    }
+  }, []);
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  return (
+    <>
+      <Flex justify="SPACE_BETWEEN" boxFill>
+        <Flex gap={{ column: 8 }}>
+          <Popover content={<NewPopover tableApi={tableApi} />}>
+            <Button disabled={pasteMode || editMode}>New</Button>
+          </Popover>
+          <Popover content={<HistoryPopover tableApi={tableApi} tableHistory={tableHistory} />} closeOption={{ keyDown: true, contentClick: true }}>
+            <Button disabled={pasteMode || editMode}>History</Button>
+          </Popover>
+          <Button disabled={!editMode} onClick={() => tableApi?.handleAddColumn()}>
+            Add Column
+          </Button>
+          <Button disabled={!editMode} onClick={() => tableApi?.handleAddRow()}>
+            Add Row
+          </Button>
+
+          <Button disabled={pasteMode} onClick={tableApi?.handleChangeEditMode}>
+            {editMode ? 'View' : 'Edit'}
+          </Button>
+        </Flex>
+        <Flex direction="ROW" gap={{ column: 8 }}>
+          <Button onClick={handleModalOpen}>Help</Button>
+          <Button disabled={editMode || pasteMode} onClick={tableApi?.handleExportCsv}>
+            Export CSV
+          </Button>
+          <Button disabled={editMode} onClick={() => tableApi?.handleChangePasteMode({ isCancel: false })}>
+            {pasteMode ? 'Done' : 'Paste'}
+          </Button>
+          {pasteMode && <Button onClick={() => tableApi?.handleChangePasteMode({ isCancel: true })}>Cancel</Button>}
+        </Flex>
       </Flex>
-      <Flex direction="ROW" gap={{ column: 8 }}>
-        <Button disabled={editMode || pasteMode} onClick={tableApi?.handleExportCsv}>
-          Export CSV
-        </Button>
-        <Button disabled={editMode} onClick={() => tableApi?.handleChangePasteMode({ isCancel: false })}>
-          {pasteMode ? 'Done' : 'Paste'}
-        </Button>
-        {pasteMode && <Button onClick={() => tableApi?.handleChangePasteMode({ isCancel: true })}>Cancel</Button>}
-      </Flex>
-    </Flex>
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        dasda
+      </Modal>
+    </>
   );
 };
 
