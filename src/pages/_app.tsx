c96a78bd0d06765b53caef2fd61886428a8d84a4
Jiho YANG <yjsm111@nuua.ai>
2023-04-12 15:28:24 +0900

    FEAT: Implement SEO and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,32 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/theme/globalStyle';
 import theme from '@/theme';
 import '../../public/font/static/style.css';
+import { DefaultSeo } from 'next-seo';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const DEFAULT_SEO = {
+    title: 'Markdown Table',
+    description: 'Generate markdown table easily',
+    canonical: 'https://www.jyangca.com',
+    openGraph: {
+      type: 'website',
+      locale: 'ko_KR',
+      url: 'Markdown Table',
+      title: 'Markdown Table',
+      site_name: 'Markdown Table',
+    },
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
+      <DefaultSeo {...DEFAULT_SEO} />
       <Head>
-        <title>Markdown Table</title>
-        <meta name="viewport" content="width=1920" />
+        <link rel="icon" type="image/png" href="https://icons-for-free.com/download-icon-markdown-1329858347762931910_512.png" />
       </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   );
